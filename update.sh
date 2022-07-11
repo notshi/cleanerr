89941cc9fd56a3e6d2d6f8215612f88971ec3e5e
shi <notshi@gmail.com>
2022-07-11 14:33:53 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -9,10 +9,10 @@ cd "$dirname"
 # rm logs.txt
 
 # if [ ! -f "logs.txt" ] ; then
-	curl -sS https://raw.githubusercontent.com/xriss/dataiati/main/logs.txt -o logs.txt
-	curl -sS https://raw.githubusercontent.com/devinit/D-Portal/master/dstore/json/packages.json -o packages.json
-	curl -sS https://raw.githubusercontent.com/xriss/dataiati/main/json/organisation-identifiers.errors.json -o organisation-identifiers.errors.json
-	curl -sS https://raw.githubusercontent.com/xriss/dataiati/main/json/activity-identifiers.errors.json -o activity-identifiers.errors.json
+	curl --show-error --silent https://raw.githubusercontent.com/xriss/dataiati/main/logs.txt -o logs.txt
+	curl --show-error --silent https://raw.githubusercontent.com/devinit/D-Portal/master/dstore/json/packages.json -o packages.json
+	curl --show-error --silent https://raw.githubusercontent.com/xriss/dataiati/main/json/organisation-identifiers.errors.json -o organisation-identifiers.errors.json
+	curl --show-error --silent https://raw.githubusercontent.com/xriss/dataiati/main/json/activity-identifiers.errors.json -o activity-identifiers.errors.json
 # fi
 
 date > date.txt
